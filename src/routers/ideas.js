483b91179fa1e0a1a9b7fc9950b51e14b61f30ac
yun_cloud <ookk8282@gmail.com>
2017-05-14 11:23:10 +0800

    [idea-like] finish

--- src/routers/ideas.js
+++ src/routers/ideas.js
@@ -14,7 +14,7 @@ router.use(accessController); // Allows cross-origin HTTP requests
 // list
 router.get('/ideas', function(req, res, next) {
 	const {searchText, order} = req.query;
-	console.log(JSON.stringify(req.query));
+	// console.log(JSON.stringify(req.query));
 	var fbID = req.get('userID');
 	if (fbID === undefined) {
 		fbID = null;
@@ -25,6 +25,27 @@ router.get('/ideas', function(req, res, next) {
 			idea.liked = (idea.liked=="1");
 		}
 		res.json(ideas);
+	}).catch(next);
+});
+
+// like
+router.post('/ideas/:i_id', function(req, res, next) {
+	const {i_id} = req.params;
+	const fb_id = req.get('userID');
+	if (fb_id === undefined) {
+        const err = new Error('like an idea need to login');
+        err.status = 401;
+        throw err;
+	}
+	if (!i_id) {
+        const err = new Error('which idea you like');
+        err.status = 400;
+        throw err;
+	}
+
+	ideasModel.like(i_id, fb_id).then(ret => {
+		// {i_id, like_number, liked: boolean}
+		res.json(ret);
 	}).catch(next);
 });
 
