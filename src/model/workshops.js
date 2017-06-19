d25fb6d6e8f69e47399e7c19cce35280f1bb1321
charlie <charlie4182@gmail.com>
2017-06-19 17:21:19 +0800

    pagination final version

--- src/model/workshops.js
+++ src/model/workshops.js
@@ -92,7 +92,7 @@ function adapter(workshop) {
 //  API below  //
 /////////////////
 
-function list(searchText, stateFilter, offset, limit) {
+function list(searchText, stateFilter, offset=0, limit=8) {
     const now = Date.now();
 
     var where = [];
